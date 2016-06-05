c6586c1461d86047fc2c94212a8768485683b640
Rodrigo Nascimento de Carvalho <rnascimentodecarva@unomaha.edu>
2016-06-05 20:56:34 -0300

    I have to consider the ocurrence of a group of 5 at any point instead of just checking the last group. I also removed the 5-tuple sets, I noticed I didn't need them.

--- src/main/java/tddpotter/Cart.java
+++ src/main/java/tddpotter/Cart.java
@@ -2,9 +2,6 @@ package tddpotter;
 
 import java.lang.reflect.Array;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.LinkedHashSet;
-import java.util.Set;
 
 /**
  * Created by rodrigonc on 05/06/16.
@@ -15,7 +12,7 @@ public class Cart {
     private Integer[] books = new Integer[]{};
     private double[] discounts = {0, 1, 0.95, 0.90, 0.80, 0.75};
     private Integer[] sameBooks = new Integer[]{0, 0, 0, 0, 0};
-    private ArrayList<Integer[]> sets = new ArrayList<Integer[]>();
+    private ArrayList<Integer> sets = new ArrayList<Integer>();
 
     public Cart() {
         this.bookPrice = 8;
@@ -26,9 +23,8 @@ public class Cart {
 
         buildBookSets();
 
-        for (Integer[] set : sets) {
-            int setSize = countBooks(set);
-            result += bookPrice * discounts[setSize] * setSize;
+        for (int set : sets) {
+            result += bookPrice * discounts[set] * set;
         }
 
         return result;
@@ -39,57 +35,27 @@ public class Cart {
         int sameBooksSize = Array.getLength(sameBooks);
 
         while (sumOfBooks > 0) {
-            Integer[] bookSet = new Integer[]{0, 0, 0, 0, 0};
             int diffCount = 0;
 
             for (int i = 0; i < sameBooksSize; i++) {
                 if (sameBooks[i] > 0) {
-                    bookSet[i]++;
                     diffCount++;
                     sameBooks[i]--;
                 }
             }
 
-            if (!sets.isEmpty()) {
-                Integer[] lastSet = sets.get(sets.size() - 1);
-                if (countBooks(bookSet) == 3 && countBooks(lastSet) == 5) {
-
-                    int diffPos = diffPos(bookSet, lastSet);
-
-                    if (diffPos != -1) {
-                        bookSet[diffPos] = lastSet[diffPos];
-                        lastSet[diffPos] = 0;
-                        sets.remove(sets.size() - 1);
-                        sets.add(lastSet);
-                    }
-                }
+            if (!sets.isEmpty() && diffCount == 3 && sets.contains(5)) {
+                int foundPos = sets.lastIndexOf(5);
+                sets.remove(foundPos);
+                sets.add(foundPos, 4);
+                sets.add(diffCount + 1);
+            } else {
+                sets.add(diffCount);
             }
 
             sumOfBooks -= diffCount;
-            sets.add(bookSet);
         }
     }
-
-    private int diffPos(Integer[] bookSet, Integer[] lastSet) {
-        for (int i = 0; i < Array.getLength(bookSet); i++) {
-            if (bookSet[i] != lastSet[i]) {
-                return i;
-            }
-        }
-        return -1;
-    }
-
-    private int countBooks(Integer[] set) {
-        int count = 0;
-
-        for (int i = 0; i < Array.getLength(set); i++) {
-            if (set[i] == 1) {
-                count++;
-            }
-        }
-        return count;
-    }
-
 
     public void addBooks(Integer[] books) {
         this.books = books;
